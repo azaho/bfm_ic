73ce583cee43039d896f5f9d8cb18563d3117262
Andrii <anzahorodnii@gmail.com>
2024-12-15 22:07:35 -0800

    added MSE loss example

--- transformer_test_train_multisub_mse.py
+++ transformer_test_train_multisub_mse.py
@@ -84,7 +84,7 @@ for subject_id, trial_id in train_subject_trials:
     dataloader = BrainTreebankDataLoader(subject_id, trial_id, trim_electrodes_to=trim_electrodes_to, device=device, batch_size=batch_size)
     dataloader_store.append(dataloader)
 
-optimizer = torch.optim.Adam(list(model.parameters()) + electrode_emb_store + [electrode_embeddings_scale], lr=0.0001, weight_decay=0.01)
+optimizer = torch.optim.Adam(list(model.parameters()) + electrode_emb_store + [electrode_embeddings_scale], lr=0.0001)
 
 loss_store = []
 emb_scale_store = []
