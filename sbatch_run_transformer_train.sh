5259aadc9b3a220ae085e97c8b35e951c9bf43b1
Andrii <anzahorodnii@gmail.com>
2024-12-19 14:45:30 -0500

    simple problem lr 001

--- sbatch_run_transformer_train.sh
+++ sbatch_run_transformer_train.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 #SBATCH -n 1                # node count
-#SBATCH --mem-per-cpu=24G    # memory per cpu-core
-#SBATCH -t 6:00:00         # total run time limit (HH:MM:SS) (increased to 24 hours)
-#SBATCH --array=0-0       # 2000 total combinations (4*5*4*5*5)
+#SBATCH --mem-per-cpu=2G    # memory per cpu-core
+#SBATCH -t 4:00:00         # total run time limit (HH:MM:SS) (increased to 24 hours)
+#SBATCH --array=0-3       # 2000 total combinations (4*5*4*5*5)
 #SBATCH --output /om/user/zaho/bfm_ic/reports/slurm-%A_%a.out # STDOUT
 #SBATCH --gres=gpu:a100:1
 export PATH="/om2/user/zaho/anaconda3/bin:/om2/user/zaho/anaconda3/condabin:$PATH"
 conda activate venv
 
 # Define arrays for each hyperparameter
-#n_top_pc_llm_array=(100 400 800 -1)
-#weight_decay_array=(0.0 0.001 0.005 0.01 0.02)
+dtype_array=('bfloat16' 'float32')
+lr_array=(0.0001 0.001)
 # Calculate indices for each hyperparameter
-#index=$SLURM_ARRAY_TASK_ID
-#n_top_pc_llm_index=$((index % 4))
+index=$SLURM_ARRAY_TASK_ID
+dtype_index=$((index % 2))
+lr_index=$((index / 2))
 
-python ttt.py
+python ttt.py --dtype ${dtype_array[dtype_index]} --lrmax ${lr_array[lr_index]} --lrmin ${lr_array[lr_index]}
