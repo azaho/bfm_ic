2fa49bd634410a2f8d83cd7836d381acec0db843
Andrii <anzahorodnii@gmail.com>
2024-12-19 23:05:36 -0500

    changed lr and epochs

--- sbatch_run_transformer_train.sh
+++ sbatch_run_transformer_train.sh
@@ -2,7 +2,7 @@
 #SBATCH -n 1                # node count
 #SBATCH --mem-per-cpu=2G    # memory per cpu-core
 #SBATCH -t 4:00:00         # total run time limit (HH:MM:SS) (increased to 24 hours)
-#SBATCH --array=0-3       # 2000 total combinations (4*5*4*5*5)
+#SBATCH --array=0-5       # 2000 total combinations (4*5*4*5*5)
 #SBATCH --output /om/user/zaho/bfm_ic/reports/slurm-%A_%a.out # STDOUT
 #SBATCH --gres=gpu:a100:1
 export PATH="/om2/user/zaho/anaconda3/bin:/om2/user/zaho/anaconda3/condabin:$PATH"
@@ -11,7 +11,7 @@ conda activate venv
 
 # Define arrays for each hyperparameter
 dtype_array=('bfloat16' 'float32')
-lr_array=(0.002 0.001)
+lr_array=(0.002 0.001 0.0005)
 # Calculate indices for each hyperparameter
 index=$SLURM_ARRAY_TASK_ID
 dtype_index=$((index % 2))
