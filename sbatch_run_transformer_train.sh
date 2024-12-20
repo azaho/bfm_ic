5739a4d75ceb1b3e313d2fe7ab29a5253fd61c2d
Andrii <anzahorodnii@gmail.com>
2024-12-20 13:23:06 -0500

    go to lower mgn

--- sbatch_run_transformer_train.sh
+++ sbatch_run_transformer_train.sh
@@ -2,7 +2,7 @@
 #SBATCH -n 1                # node count
 #SBATCH --mem-per-cpu=2G    # memory per cpu-core
 #SBATCH -t 5:00:00         # total run time limit (HH:MM:SS) (increased to 24 hours)
-#SBATCH --array=0-8      # 2000 total combinations (4*5*4*5*5)
+#SBATCH --array=0-5      # 2000 total combinations (4*5*4*5*5)
 #SBATCH --output /om/user/zaho/bfm_ic/reports/slurm-%A_%a.out # STDOUT
 #SBATCH --gres=gpu:a100:1
 
@@ -12,14 +12,14 @@ conda activate venv
 
 # Define arrays for each hyperparameter
 dtype_array=('bfloat16')
-lr_array=(0.001 0.0005 0.0001)
-wd_array=(0.000 0.001 0.0001)
-max_gradient_norm_array=(-1 1 0.5)
+lr_array=(0.001 0.002)
+wd_array=(0.000)
+max_gradient_norm_array=(0.5 0.2 0.1)
 # Calculate indices for each hyperparameter
 index=$SLURM_ARRAY_TASK_ID
-dtype_index=$((index / 27))
-lr_index=$(((index % 27) / 9))
-wd_index=$(((index % 9) / 3))
+dtype_index=1
+wd_index=1
+lr_index=$(((index / 3))
 max_gradient_norm_index=$((index % 3))
 
 python ttt.py --dtype ${dtype_array[dtype_index]} --lrmax ${lr_array[lr_index]} --lrmin ${lr_array[lr_index]} --weight_decay ${wd_array[wd_index]} --max_gradient_norm ${max_gradient_norm_array[max_gradient_norm_index]} --wait_n_intervals $SLURM_ARRAY_TASK_ID
