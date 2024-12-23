50b846343f1242e4bc1d747cea10fc1b10664a70
Andrii <anzahorodnii@gmail.com>
2024-12-23 17:45:59 -0500

    added bench chunking code

--- sbatch_run_steroid_allsub_cpc.sh
+++ sbatch_run_steroid_allsub_cpc.sh
@@ -3,7 +3,7 @@
 #SBATCH -n 1                # node count
 #SBATCH --mem-per-cpu=16G    # memory per cpu-core
 #SBATCH -t 16:00:00         # total run time limit (HH:MM:SS) (increased to 24 hours)
-#SBATCH --array=0-17      # 66 total combinations (2*2*2*2*2*2)
+#SBATCH --array=0-5      # 66 total combinations (2*2*2*2*2*2)
 #SBATCH --output /shared/anzah/bfm_ic/reports/%A_%a.out # STDOUT
 #SBATCH --gres=gpu:1
 
@@ -16,10 +16,11 @@ source .venv/bin/activate
 dtype_array=('bfloat16')
 optimizer_array=('Muon')
 electrode_init_array=('normal')
-dropout_array=(0.0)
+dropout_array=(0.2)
 batch_size_array=(100)
-subjects_array=('1' '2' '12' '123' '1234567890' '12345')
-lr_array=(0.001 0.002 0.0005)
+subjects_array=('2' '1234567890')
+lr_array=(0.001)
+nl_array=(10 15 20)
 # Fixed parameters
 wd=0
 max_gradient_norm=-1
@@ -28,15 +29,16 @@ dropout_index=0
 batch_size_index=0
 optimizer_index=0
 electrode_init_index=0
+lr_index=0
+random_string='1337'
 
 # Calculate indices for each hyperparameter
 index=$SLURM_ARRAY_TASK_ID
-lr_index=$((index % 3))
-subjects_index=$(((index / 3) % 6))
-random_string=$((index / 3 / 6))
+subjects_index=$((index % 2))
+nl_index=$((index / 2))
 
 # no need to run through srun because it's already an array job on a single node
 python ttt_cpc.py --dtype ${dtype_array[dtype_index]} --optimizer ${optimizer_array[optimizer_index]} \
 --electrode_embedding_init ${electrode_init_array[electrode_init_index]} --dr ${dropout_array[dropout_index]} \
 --bs ${batch_size_array[batch_size_index]} --lrmax ${lr_array[lr_index]} --lrmin ${lr_array[lr_index]} --weight_decay $wd --max_gradient_norm $max_gradient_norm \
---subjects ${subjects_array[subjects_index]} --wait_n_intervals $SLURM_ARRAY_TASK_ID --wandb_project bfm --rs $random_string --wandb_project bfm_steroids_allsub3
+--subjects ${subjects_array[subjects_index]} --wait_n_intervals $SLURM_ARRAY_TASK_ID --wandb_project bfm --rs $random_string --wandb_project bfm_steroids_allsub3 --nl ${nl_array[nl_index]}
