2eea783eebc4e1697526d194550c23bde62aaadd
Andrii <anzahorodnii@gmail.com>
2024-12-23 20:02:27 -0500

    added eval to training

--- sbatch_run_steroid_allsub_cpc.sh
+++ sbatch_run_steroid_allsub_cpc.sh
@@ -30,7 +30,7 @@ batch_size_index=0
 optimizer_index=0
 electrode_init_index=0
 lr_index=0
-random_string='1338'
+random_string='1337'
 
 # Calculate indices for each hyperparameter
 index=$SLURM_ARRAY_TASK_ID
