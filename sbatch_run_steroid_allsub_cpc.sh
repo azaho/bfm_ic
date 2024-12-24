cd7c37773d0eeff6949227cec8588406e34c8d06
Andrii <anzahorodnii@gmail.com>
2024-12-23 19:58:54 -0500

    added eval to training

--- sbatch_run_steroid_allsub_cpc.sh
+++ sbatch_run_steroid_allsub_cpc.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=brain_foundation_model_training          # Name of the job
 #SBATCH -n 1                # node count
-#SBATCH --mem-per-cpu=16G    # memory per cpu-core
+#SBATCH --mem-per-cpu=64G    # memory per cpu-core
 #SBATCH -t 16:00:00         # total run time limit (HH:MM:SS) (increased to 24 hours)
 #SBATCH --array=0-5      # 66 total combinations (2*2*2*2*2*2)
 #SBATCH --output /shared/anzah/bfm_ic/reports/%A_%a.out # STDOUT
@@ -20,12 +20,12 @@ dropout_array=(0.0 0.1 0.2)
 batch_size_array=(100)
 subjects_array=('2' '1234567890')
 lr_array=(0.001)
-nl_array=(10 15 20)
+nl_array=(15)
 # Fixed parameters
 wd=0
 max_gradient_norm=-1
 dtype_index=0
-nl_index=1
+nl_index=0
 batch_size_index=0
 optimizer_index=0
 electrode_init_index=0
