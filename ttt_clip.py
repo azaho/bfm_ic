6eaf1c85e10384bccd49813bfaabbc0de1005a78
Andrii <anzahorodnii@gmail.com>
2024-12-26 03:51:04 -0500

    added next token pred clip lsos

--- ttt_clip.py
+++ ttt_clip.py
@@ -96,7 +96,7 @@ assert ('lr_warmup_frac' in training_config) != ('lr_warmup_steps' in training_c
 wandb_log = (len(args.wandb_project) > 0)
 
 transformer_config = {
-    'model_name': "clip2",
+    'model_name': "clip3",
     'max_n_electrodes': 128,#158,
     'n_freq_features': 37,
     'max_n_time_bins': 24, # 3 second of time (every bin is 125 ms)
