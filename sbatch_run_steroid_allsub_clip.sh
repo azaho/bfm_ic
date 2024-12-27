cc929eb844e3cd9a32fd32ab12ab61e1ee3a73ef
Andrii <anzahorodnii@gmail.com>
2024-12-27 14:01:19 -0500

    zero in on NE success

--- sbatch_run_steroid_allsub_clip.sh
+++ sbatch_run_steroid_allsub_clip.sh
@@ -54,10 +54,12 @@ for gpu_id in {0..7}; do
     nl_index=$((index / 36))
     d_model_index=$((nl_index))
 
-    srun --exclusive -n1 --cpus-per-task=16 --mem=128G --gres=gpu:1 python ${filename_array[filename_index]} --dtype ${dtype_array[dtype_index]} --optimizer ${optimizer_array[optimizer_index]} \
-    --electrode_embedding_init ${electrode_init_array[electrode_init_index]} --dr ${dropout_array[dropout_index]} --dm ${d_model_array[d_model_index]} --pushaway ${pushaway_array[pushaway_index]} \
-    --bs ${batch_size_array[batch_size_index]} --lrmax ${lr_array[lr_index]} --lrmin 0.0 --weight_decay $wd --max_gradient_norm $max_gradient_norm \
-    --subjects ${subjects_array[subjects_index]} --wait_n_intervals $index --wandb_project bfm --rs ${random_string_array[random_string_index]} --wandb_project bfm_clip3 --nl ${nl_array[nl_index]} &
+    echo "python ${filename_array[filename_index]} --dtype ${dtype_array[dtype_index]} --optimizer ${optimizer_array[optimizer_index]} --electrode_embedding_init ${electrode_init_array[electrode_init_index]} --dr ${dropout_array[dropout_index]} --dm ${d_model_array[d_model_index]} --pushaway ${pushaway_array[pushaway_index]} --bs ${batch_size_array[batch_size_index]} --lrmax ${lr_array[lr_index]} --lrmin 0.0 --weight_decay $wd --max_gradient_norm $max_gradient_norm --subjects ${subjects_array[subjects_index]} --wait_n_intervals $index --wandb_project bfm --rs ${random_string_array[random_string_index]} --wandb_project bfm_clip3 --nl ${nl_array[nl_index]}"
+
+    #srun --exclusive -n1 --cpus-per-task=16 --mem=128G --gres=gpu:1 python ${filename_array[filename_index]} --dtype ${dtype_array[dtype_index]} --optimizer ${optimizer_array[optimizer_index]} \
+    #--electrode_embedding_init ${electrode_init_array[electrode_init_index]} --dr ${dropout_array[dropout_index]} --dm ${d_model_array[d_model_index]} --pushaway ${pushaway_array[pushaway_index]} \
+    #--bs ${batch_size_array[batch_size_index]} --lrmax ${lr_array[lr_index]} --lrmin 0.0 --weight_decay $wd --max_gradient_norm $max_gradient_norm \
+    #--subjects ${subjects_array[subjects_index]} --wait_n_intervals $index --wandb_project bfm --rs ${random_string_array[random_string_index]} --wandb_project bfm_clip3 --nl ${nl_array[nl_index]} &
 done
 
 wait # Wait for all parallel processes to complete
