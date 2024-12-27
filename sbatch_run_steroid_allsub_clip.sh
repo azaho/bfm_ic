d900681a96472b4f0a185504d7cf2ae3231ed90b
Andrii <anzahorodnii@gmail.com>
2024-12-27 14:44:59 -0500

    zero in on NE success

--- sbatch_run_steroid_allsub_clip.sh
+++ sbatch_run_steroid_allsub_clip.sh
@@ -5,7 +5,7 @@
 #SBATCH --gpus-per-task=1
 #SBATCH --mem-per-gpu=128G
 #SBATCH -t 16:00:00         # total run time limit (HH:MM:SS) (increased to 24 hours)
-#SBATCH --array=0-1      # 14 jobs (108/8 rounded up)
+#SBATCH --array=0-11      # 14 jobs (108/8 rounded up)
 #SBATCH --output /shared/anzah/bfm_ic/reports/%A_%a.out # STDOUT
 
 source .venv/bin/activate
@@ -22,7 +22,7 @@ lr_array=(0.001 0.0015)
 nl_array=(10 14)
 d_model_array=(192 384)
 pushaway_array=(0)
-random_string_array=('NEx')
+random_string_array=('NE')
 wd_array=(0 0.0001)
 # Fixed parameters
 max_gradient_norm=-1
@@ -40,11 +40,6 @@ optimizer_index=0
 # Run 8 combinations in parallel using all 8 GPUs
 for gpu_id in {0..7}; do
     index=$((base_index + gpu_id))
-    
-    # Skip if we've exceeded total combinations
-    if [ $index -ge 108 ]; then
-        continue
-    fi
 
     # Calculate indices for each hyperparameter
     lr_index=$((index % 2))
@@ -55,15 +50,12 @@ for gpu_id in {0..7}; do
     dropout_index=$((index / 32 % 3))
     d_model_index=$((nl_index))
 
-    if [ $subjects_index -ne 1 ]; then
-        continue
-    fi
     echo "python ${filename_array[filename_index]} --dtype ${dtype_array[dtype_index]} --optimizer ${optimizer_array[optimizer_index]} --electrode_embedding_init ${electrode_init_array[electrode_init_index]} --dr ${dropout_array[dropout_index]} --dm ${d_model_array[d_model_index]} --pushaway ${pushaway_array[pushaway_index]} --bs ${batch_size_array[batch_size_index]} --lrmax ${lr_array[lr_index]} --lrmin 0.0 --weight_decay $wd --max_gradient_norm $max_gradient_norm --subjects ${subjects_array[subjects_index]} --wait_n_intervals $index --wandb_project bfm --rs ${random_string_array[random_string_index]} --wandb_project bfm_clip3 --nl ${nl_array[nl_index]}"
 
     srun --exclusive -n1 python ${filename_array[filename_index]} --dtype ${dtype_array[dtype_index]} --optimizer ${optimizer_array[optimizer_index]} \
     --electrode_embedding_init ${electrode_init_array[electrode_init_index]} --dr ${dropout_array[dropout_index]} --dm ${d_model_array[d_model_index]} --pushaway ${pushaway_array[pushaway_index]} \
     --bs ${batch_size_array[batch_size_index]} --lrmax ${lr_array[lr_index]} --lrmin 0.0 --weight_decay ${wd_array[wd_index]} --max_gradient_norm $max_gradient_norm \
-    --subjects ${subjects_array[subjects_index]} --wait_n_intervals $index --wandb_project bfm --rs ${random_string_array[random_string_index]} --wandb_project bfm_clip3 --nl ${nl_array[nl_index]} &
+    --subjects ${subjects_array[subjects_index]} --wait_n_intervals $index --wandb_project bfm --rs ${random_string_array[random_string_index]} --wandb_project bfm_clip4 --nl ${nl_array[nl_index]} &
 done
 
 wait # Wait for all parallel processes to complete
