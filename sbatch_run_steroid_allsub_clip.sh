cc88caefcb4eaabe41556ac3bfcc0a204267aea6
Andrii <anzahorodnii@gmail.com>
2024-12-27 14:46:14 -0500

    zero in on NE success

--- sbatch_run_steroid_allsub_clip.sh
+++ sbatch_run_steroid_allsub_clip.sh
@@ -5,7 +5,7 @@
 #SBATCH --gpus-per-task=1
 #SBATCH --mem-per-gpu=128G
 #SBATCH -t 16:00:00         # total run time limit (HH:MM:SS) (increased to 24 hours)
-#SBATCH --array=0-11      # 14 jobs (108/8 rounded up)
+#SBATCH --array=0-17      # 14 jobs (108/8 rounded up)
 #SBATCH --output /shared/anzah/bfm_ic/reports/%A_%a.out # STDOUT
 
 source .venv/bin/activate
@@ -47,7 +47,7 @@ for gpu_id in {0..7}; do
     wd_index=$((index / 4 % 2))
     subjects_index=$((index / 8 % 2))
     electrode_init_index=$((index / 16 % 3))
-    dropout_index=$((index / 32 % 3))
+    dropout_index=$((index / 48 % 3))
     d_model_index=$((nl_index))
 
     echo "python ${filename_array[filename_index]} --dtype ${dtype_array[dtype_index]} --optimizer ${optimizer_array[optimizer_index]} --electrode_embedding_init ${electrode_init_array[electrode_init_index]} --dr ${dropout_array[dropout_index]} --dm ${d_model_array[d_model_index]} --pushaway ${pushaway_array[pushaway_index]} --bs ${batch_size_array[batch_size_index]} --lrmax ${lr_array[lr_index]} --lrmin 0.0 --weight_decay $wd --max_gradient_norm $max_gradient_norm --subjects ${subjects_array[subjects_index]} --wait_n_intervals $index --wandb_project bfm --rs ${random_string_array[random_string_index]} --wandb_project bfm_clip3 --nl ${nl_array[nl_index]}"
