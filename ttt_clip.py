7a5c9ea7271fa92831c19a209e1a8e586bcfeac0
azaho <anzahorodnii@gmail.com>
2024-12-29 20:57:12 -0500

    added deep fine tuning test

--- ttt_clip.py
+++ ttt_clip.py
@@ -97,7 +97,7 @@ wandb_log = (len(args.wandb_project) > 0)
 
 transformer_config = {
     'model_name': "t", # x is for loss addon, c is default clip, t is for testing deep fine tuning (no loss addon)
-    'max_n_electrodes': 128,#158,
+    'max_n_electrodes': 158,#158,
     'n_freq_features': 37,
     'max_n_time_bins': 24, # 3 second of time (every bin is 125 ms)
     'd_model': args.dm,
