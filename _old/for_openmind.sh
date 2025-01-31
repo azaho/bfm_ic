2e7e468d320e9ffd407f6470906413322354cb7e
Andrii Zahorodnii <zaho@openmind7.MIT.EDU>
2025-01-30 23:26:34 -0500

    openmind cleanup

--- _old/for_openmind.sh
+++
@@ -1,4 +0,0 @@
-cd ../../om/user/zaho/bfm_ic/
-git pull
-
-srun -n 1 --gres=gpu:a100:1 -t 16:00:00 --mem 40G --pty bash
--- _old/update_training_results_data.bat
+++
@@ -1,2 +0,0 @@
-cd training_results
-scp -r zaho@openmind.mit.edu:"/om/user/zaho/bfm_ic/training_results/*" .
